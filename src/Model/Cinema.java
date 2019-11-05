1a454adbad1b0197813469209a6a1b9faf52458b
randynyl <56857427+randynyl@users.noreply.github.com>
2019-11-05 22:55:18 +0800

    update

--- src/Model/Cinema.java
+++ src/Model/Cinema.java
@@ -1,4 +1,14 @@
 
 public class Cinema {
-
+	private Cineplex cineplex;
+	private CinemaClass cinemaClass;
+	private int code;
+	private double price;
+	private Showtime[] showtimes;
+	private SeatLayout seatLayout;
+	
+	public Cinema(Cineplex cineplex, CinemaClass cinemaClass, int code, double price, SeatLayout seatLayout) {
+		this.cineplex = cineplex;
+		this.
+	}
 }
